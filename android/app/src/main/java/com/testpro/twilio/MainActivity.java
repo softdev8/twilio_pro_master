95c855304e3091a1240eab25895a8e2add8c7fe5
softdev8 <oncemoreforyou@outlook.com>
2020-04-20 12:06:02 +0300

    changed project name

--- android/app/src/main/java/com/testpro/twilio/MainActivity.java
+++ android/app/src/main/java/com/testpro/twilio/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeTwilioExampleApp";
+        return "TwilioTestPro";
     }
 
     @Override
